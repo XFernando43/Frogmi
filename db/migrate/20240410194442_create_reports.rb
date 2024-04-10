b06e36a8be654d35a9943703b346ad48498b162d
XFernando43 <U20171E997@upc.edu.pe>
2024-04-10 14:44:56 -0500

    add: report model

---
+++ db/migrate/20240410194442_create_reports.rb
@@ -0,0 +1,22 @@
+class CreateReports < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reports do |t|
+      t.string :Reportid
+      t.float :mag
+      t.string :place
+      t.string :time
+      t.string :Long
+      t.string :Integer
+      t.string :url
+      t.integer :tsunami
+      t.string :magType
+      t.string :title
+      t.string :string
+      t.string :longitude
+      t.string :float
+      t.float :latitude
+
+      t.timestamps
+    end
+  end
+end
