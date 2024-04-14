bf651123c2645a68d4f0ef0a50c9216206c29ccb
XFernando43 <U20171E997@upc.edu.pe>
2024-04-14 12:47:04 -0500

    add: get comments by reportID

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,12 +1,10 @@
-
-module Api 
-
+module Api
   module V1
     class CommentsController < ApplicationController  
-      
+      before_action :set_comment, only: [:show]
       skip_before_action :verify_authenticity_token, only: [:create]
 
-
+      #get all
       def index
         comments = Comment.all
         render json: {
@@ -16,27 +14,18 @@ module Api
         }, status: :ok
       end
 
-      # Método para obtener comentarios por report_id
-      def index_by_report
-        report_id = params[:report_id]
-        comments = Comment.where(report_id: report_id)
-        
-        if comments.any?
-          render json: {
-            status: 'exitoso papi',
-            message: "comentarios encontrados para el reporte con ID #{report_id}",
-            comments: comments
-          }, status: :ok
-        else
-          render json: {
-            status: 'error',
-            message: "No se encontraron comentarios para el reporte con ID #{report_id}"
-          }, status: :not_found
-        end
+      #get comment By iD
+      def show
+        render json: {
+          status: 'success',
+          message: 'comment obtained successfully',
+          data: @comment
+        }, status: :ok
       end
 
+      #create comentario
       def create
-        report_id = params[:report_id] # Aquí obtenemos el report_id de los parámetros de la solicitud
+        report_id = params[:report_id]
         comment = Comment.new(body: "HOLIWI", report_id: report_id)
         if comment.save
           render json: {
@@ -52,40 +41,23 @@ module Api
           }, status: :unprocessable_entity
         end
       end
-      
-
 
       
+      private
+      def set_comment
+        @comment = Comment.where(report_id: params[:id])
+        
+        if @comment.empty?
+          render json: {
+            status: 'error',
+            message: 'comments not found for report_id'
+          }, status: :not_found
+        end
+
+      end
+
+
+
     end
   end 
-
-end  
-
-
-
-
-# comment = Comment.new(comment_params)
-        
-        # if comment.save
-        #   render json: {
-        #     status: "exitoso",
-        #     message: "Comentario creado",
-        #     data: comment
-        #   }, status: :ok
-        # else
-        #   render json: {
-        #     status: "Fallido",
-        #     message: "Comentario no creado",
-        #     errors: comment.errors.full_messages
-        #   }, status: :unprocessable_entity
-        # end
-
-
-      # private
-      
-      # def comment_params
-      #   params.require(:comment).permit(:body, :report_id)
-      # end
-      
-
-
+end
