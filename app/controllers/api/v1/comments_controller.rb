e559b2713c849d0e0ff361e2350316f2e2955a29
XFernando43 <U20171E997@upc.edu.pe>
2024-04-15 11:02:04 -0500

    fix: error with comments api

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,32 +1,23 @@
 module Api
   module V1
     class CommentsController < ApplicationController  
-      before_action :set_comment, only: [:show]
       skip_before_action :verify_authenticity_token, only: [:create]
 
       #get all
       def index
-        comments = Comment.all
-        render json: {
-          status: 'exitoso papi',
-          message: 'comentarios encontrados',
-          comments: comments
-        }, status: :ok
-      end
-
-      #get comment By iD
-      def show
+        reportId = params[:report_id]
+        comments = Comment.where(report_id: reportId)
         render json: {
           status: 'success',
-          message: 'comment obtained successfully',
-          data: @comment
+          message: 'comentarios encontrados',
+          comments: comments
         }, status: :ok
       end
 
       # create comentario
       def create
         report_id = params[:report_id]
-        body = params[:comment] # Aquí obtenemos directamente el valor del cuerpo del comentario
+        body = params[:comment]
         comment = Comment.new(body: body, report_id: report_id)
         
         if comment.save
@@ -44,17 +35,6 @@ module Api
         end
       end
 
-      private
-      def set_comment
-        @comment = Comment.where(report_id: params[:report_id])
-        
-        if @comment.empty?
-          render json: {
-            status: 'error',
-            message: 'comments not found for report_id'
-          }, status: :not_found
-        end
-      end
     end
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :reports
-      resources :comments, only: [:index, :create], path: '/comments/:report_id'
+      resources :comments, path: '/comments/:report_id'
     end
   end
 end
